08804167fdf9902bad6ec1a7fd7f1d22968f08d9
Tridib Bose <84838320+bosetridib@users.noreply.github.com>
2024-12-19 19:49:42 +0200

    ...

--- NonParametricIRF_Estimation.py
+++ NonParametricIRF_Estimation.py
@@ -12,8 +12,8 @@ y = df_mod.copy()
 # dataplot(y)
 # y = y[y.columns[1:8]]
 # VAR analysis
-# model_var = sm.tsa.VAR(y)
-# results_var = model_var.fit(6)
+model_var = sm.tsa.VAR(y)
+results_var = model_var.fit(6)
 # results_var.irf(40).plot(); plt.show()
 # results_var.irf(40).plot_cum_effects(); plt.show()
 # Usual and orthogonal IRFs (use 0:temperature, 2:cpu_index )
--- NonParametricIRF_Forecasting_GIRF.py
+++ NonParametricIRF_Forecasting_GIRF.py
@@ -22,39 +22,42 @@ delta = B_mat[:,0]
 # Set the period of the of interest
 histoi = y.index.date[-1]
 
-# Set the history of interest upto and NOT including the period
+# Set the history of interest upto and NOT including the period (T-1)
 omega = y_normalized.loc[:histoi - pd.DateOffset(months = 1)]
-X_train = y_normalized.loc[:histoi]
-T = X_train.shape[0]
-# The X_train contains observations upto and including the period T.
+# Period of interest's realized data at T
+omega_mutated = y_normalized.loc[str(histoi)]
+T = omega.shape[0] + 1
 
 # Fit the knn upto and NOT including the history of interest
 knn.fit(omega)
 
 # Find the nearest neighbours and their distance from period of interest.
-dist, ind = knn.kneighbors(X_train.loc[str(histoi)].to_numpy().reshape(1,-1))
+dist, ind = knn.kneighbors(omega_mutated.to_numpy().reshape(1,-1))
 dist = dist[0,:]; ind = ind[0,:]
 dist = (dist - dist.min())/(dist.max() - dist.min())
 weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
 
 # Map the lead indices of the nearest neighbours
 lead_index = omega.iloc[ind].index + pd.DateOffset(months=1)
-X_train_lead = X_train.loc[lead_index]
-y_f = np.matmul(X_train_lead.T, weig).to_frame().T
+omega_lead = pd.concat([omega, omega_mutated.to_frame().T], axis=0).loc[lead_index]
+y_f = np.matmul(omega_lead.T, weig).to_frame().T
 # This is the forecast for the period of interest, ie. E(y_T) at h=0
 
-# The following would be the forecast of the y_T+1,+ 2,...H
+# The following would be the forecast of the y_T+1,+2,...H
 # The principle is same as before. Fit the data upto and NOT including the final period. Find
 # nearest neighbour of the final period and estimate the forecast. Update the data with the
 # forecasts, and repeat.
 for h in range(1,H+1):
     omega_updated = pd.concat([omega,y_f], axis=0)
-    knn.fit(omega_updated.iloc[:-1])
-    dist, ind = knn.kneighbors(y_f.iloc[-1].to_numpy().reshape(1,-1))
+    X_train = pd.concat([omega,y_f], axis=0)
+    knn.fit(omega_updated)
+    dist, ind = knn.kneighbors(X_train.iloc[-1].to_numpy().reshape(1,-1))
     dist = dist[0,:]; ind = ind[0,:]
     dist = (dist - dist.min())/(dist.max() - dist.min())
     weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
-    lead_index = omega_updated.iloc[ind].index + pd.DateOffset(months=1)
+    lead_index = np.array(
+        [i+1 if type(i) == int else i + pd.DateOffset(months=1) for i in omega_updated.iloc[ind].index]
+    )
     X_train_lead = X_train.loc[lead_index]
     y_f.loc[h] = np.matmul(X_train_lead.T, weig).values
 
@@ -67,28 +70,48 @@ for h in range(1,H+1):
 # y_fvar.cumsum().plot(subplots=True, layout=(2,4)); plt.show()
 
 # GIRFs
-# Updated history
-omega_star = y_normalized.loc[histoi] + delta
-knn.fit(X_train)
+omega = y_normalized.loc[:histoi - pd.DateOffset(months = 1)]
+X_train = y_normalized.loc[:histoi]
+# The X_train contains observations upto and including the period T.
+
+# The period of interest with shock
+omega_star = y_normalized.loc[str(histoi)] + delta
+
+# Fit the knn upto and NOT including the history of interest
+knn.fit(omega)
+
+# Find the nearest neighbours and their distance from period of interest.
 dist, ind = knn.kneighbors(omega_star.to_numpy().reshape(1,-1))
 dist = dist[0,:]; ind = ind[0,:]
 dist = (dist - dist.min())/(dist.max() - dist.min())
 weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
-girf = np.matmul(pd.concat([X_train , y_normalized.iloc[-1].to_frame().T], axis=0).iloc[ind+1].T, weig).to_frame().T
 
+# Map the lead indices of the nearest neighbours
+lead_index = omega.iloc[ind].index + pd.DateOffset(months=1)
+X_train_lead = X_train.loc[lead_index]
+y_f_delta = np.matmul(X_train_lead.T, weig).to_frame().T
+# This is the forecast for the period of interest with shock, ie. E(y_T, delta) at h=0
+
+# The following would be the forecast of the y_T+1,+ 2,...H with shock
+# The principle is same as in forecasts.
 for h in range(1,H+1):
-    knn.fit(pd.concat([X_train, girf], axis=0).iloc[:-1])
-    dist, ind = knn.kneighbors(girf.iloc[-1].to_numpy().reshape(1,-1))
+    omega_updated = pd.concat([omega,y_f_delta], axis=0).iloc[:-1]
+    X_train = pd.concat([omega,y_f_delta], axis=0)
+    knn.fit(omega_updated)
+    dist, ind = knn.kneighbors(X_train.iloc[-1].to_numpy().reshape(1,-1))
     dist = dist[0,:]; ind = ind[0,:]
     dist = (dist - dist.min())/(dist.max() - dist.min())
     weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
-    girf.loc[h] = np.matmul(pd.concat([X_train, girf], axis=0).iloc[ind+1].T, weig).values
+    lead_index = np.array([i+1 if type(i) == int else i + pd.DateOffset(months=1) for i in omega_updated.iloc[ind].index])
+    X_train_lead = X_train.loc[lead_index]
+    y_f_delta.loc[h] = np.matmul(X_train_lead.T, weig).values
 
+# dataplot(y_f_delta - y_f)
 # Set R: the number of simulations
 R = 100
 
 # GIRF_star and y_f_star at H=0
-girf_star_df = pd.DataFrame(columns=girf.columns)
+y_f_delta_star_df = pd.DataFrame(columns=y_f_delta.columns)
 y_f_star_df = pd.DataFrame(columns=y_f.columns)
 
 for i in range(0,R):
@@ -109,18 +132,18 @@ for i in range(0,R):
     dist = dist[0,:]; ind = ind[0,:]
     dist = (dist - dist.min())/(dist.max() - dist.min())
     weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
-    girf_star_df = pd.concat([girf_star_df, np.matmul(X_train_ci_lead1.iloc[ind].T, weig).to_frame().T])
+    y_f_delta_star_df = pd.concat([y_f_delta_star_df, np.matmul(X_train_ci_lead1.iloc[ind].T, weig).to_frame().T])
 
-girf_star_df = girf_star_df - y_f_star_df
+girf_star_df = y_f_delta_star_df - y_f_star_df
 
 # Confidence level
 conf = 0.90
 
 # Multi-Index GIRF
-girf_complete = pd.concat([2*(girf.iloc[0] - y_f.iloc[0]) - girf_star_df.quantile(conf+(1-conf)/2), girf.iloc[0] - y_f.iloc[0], 2*(girf.iloc[0] - y_f.iloc[0]) - girf_star_df.quantile((1-conf)/2)], axis=1).T
+girf_complete = pd.concat([2*(y_f_delta.iloc[0] - y_f.iloc[0]) - girf_star_df.quantile(conf+(1-conf)/2), y_f_delta.iloc[0] - y_f.iloc[0], 2*(y_f_delta.iloc[0] - y_f.iloc[0]) - girf_star_df.quantile((1-conf)/2)], axis=1).T
 
 for h in range(1,H+1):
-    girf_star_df = pd.DataFrame(columns=girf.columns)
+    y_f_delta_star_df = pd.DataFrame(columns=y_f_delta.columns)
     y_f_star_df = pd.DataFrame(columns=y_f.columns)
     for i in range(0,R):
         X_train_ci = X_train.sample(n = T, replace=True).sort_index()
@@ -136,18 +159,21 @@ for h in range(1,H+1):
         X_train_ci = X_train.sample(n = T, replace=True).sort_index()
         X_train_ci_lead1 = y_normalized.loc[X_train_ci.index + pd.DateOffset(months=1)]
         knn.fit(X_train_ci)
-        dist, ind = knn.kneighbors(girf.iloc[h-1].to_numpy().reshape(1,-1))
+        dist, ind = knn.kneighbors(y_f_delta.iloc[h-1].to_numpy().reshape(1,-1))
         dist = dist[0,:]; ind = ind[0,:]
         dist = (dist - dist.min())/(dist.max() - dist.min())
         weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
-        girf_star_df = pd.concat([girf_star_df, np.matmul(X_train_ci_lead1.iloc[ind].T, weig).to_frame().T])
-    girf_star_df = girf_star_df - y_f_star_df
+        y_f_delta_star_df = pd.concat([girf_star_df, np.matmul(X_train_ci_lead1.iloc[ind].T, weig).to_frame().T])
+    girf_star_df = y_f_delta_star_df - y_f_star_df
     girf_complete = pd.concat([
-        girf_complete, pd.concat([2*(girf.iloc[h-1] - y_f.iloc[h-1]) - girf_star_df.quantile(conf+(1-conf)/2), girf.iloc[h-1] - y_f.iloc[h-1], 2*(girf.iloc[h-1] - y_f.iloc[h-1]) - girf_star_df.quantile((1-conf)/2)], axis=1).T
+        girf_complete, pd.concat([2*(y_f_delta.iloc[h-1] - y_f.iloc[h-1]) - girf_star_df.quantile(conf+(1-conf)/2), y_f_delta.iloc[h-1] - y_f.iloc[h-1], 2*(y_f_delta.iloc[h-1] - y_f.iloc[h-1]) - girf_star_df.quantile((1-conf)/2)], axis=1).T
     ])
 
 girf_complete.index = pd.MultiIndex(levels=[range(0,H+1),['lower','GIRF','upper']], codes=[[x//3 for x in range(0,41*3)],[0,1,2]*41], names=('Horizon', 'CI'))
 girf_complete
+
+girf = y_f_delta - y_f
+girf_cumul = girf.cumsum(axis=0)
 
 girf_complete[[y.columns[2]]].unstack().plot(); plt.show()
 girf[[y.columns[2]]].plot(); plt.show()
@@ -157,9 +183,6 @@ girf[[y.columns[4]]].plot(); plt.show()
 
 girf_complete[[y.columns[3]]].unstack().cumsum().plot(); plt.show()
 girf[[y.columns[3]]].plot(); plt.show()
-
-girf = girf - y_f
-girf_cumul = girf.cumsum(axis=0)
 
 girf = pd.DataFrame(robust_transformer.inverse_transform(girf), columns=girf.columns)
 dataplot(girf)
