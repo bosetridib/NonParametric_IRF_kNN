da35f8b6cc4c315d8c73fd535eccd8beb8cf7de9
bosetridib <bosetridib@yahoo.com>
2025-01-16 23:02:44 +0200

    ...

--- NonParametricIRF_Forecasting_GIRF_original.py
+++ NonParametricIRF_Forecasting_GIRF_original.py
@@ -15,10 +15,19 @@ df_std = (df - df.mean())/df.std()
 # Forecasting
 # Horizon "in the middle"
 H = 40
-k = df_std.shape[0] - H
-
 omega = df_std.iloc[:-1]
-histoi = df_std.index[-1]
+histoi = df_std.iloc[-1]
 
+k = omega.shape[0]
 knn = NearestNeighbors(n_neighbors=k, metric='euclidean')
+
+# Estimate y_T
+knn.fit(omega)
+
+dist, ind = knn.kneighbors(histoi.to_numpy().reshape(1,-1))
+dist = dist[0,:]; ind = ind[0,:]
+dist = (dist - dist.min())/(dist.max() - dist.min())
+weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
+# Estimated (NOT forecasted) the period of interest T
+y_f = np.matmul(omega.iloc[ind].T, weig).to_frame().T
 
