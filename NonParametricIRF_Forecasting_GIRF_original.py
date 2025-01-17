9ba29c768c161c456e393f3d05331a3139342fca
bosetridib <bosetridib@yahoo.com>
2025-01-17 12:30:26 +0200

    ...

--- NonParametricIRF_Forecasting_GIRF_original.py
+++ NonParametricIRF_Forecasting_GIRF_original.py
@@ -2,6 +2,7 @@
 from NonParametricIRF_Data import *
 from Functions_Required import *
 from sklearn.neighbors import NearestNeighbors
+from scipy.spatial.distance import euclidean
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -22,12 +23,22 @@ k = omega.shape[0]
 knn = NearestNeighbors(n_neighbors=k, metric='euclidean')
 
 # Estimate y_T
-knn.fit(omega)
-
-dist, ind = knn.kneighbors(histoi.to_numpy().reshape(1,-1))
-dist = dist[0,:]; ind = ind[0,:]
+dist = np.array([euclidean(omega.loc[i], histoi) for i in omega.index])
 dist = (dist - dist.min())/(dist.max() - dist.min())
 weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
 # Estimated (NOT forecasted) the period of interest T
-y_f = np.matmul(omega.iloc[ind].T, weig).to_frame().T
+y_f = np.matmul(omega.T, weig).to_frame().T
+y_f = np.matmul(df.iloc[:-1].T, weig).to_frame().T
+
+u = df.iloc[:-1] - y_f.values.squeeze()
+u = u.multiply(weig, axis = 0)
+u.mean()
+
+# df.iloc[:-1].plot(
+#     subplots=True, layout=(2,4), color = 'blue',
+#     ax=u.plot(
+#         subplots=True, layout=(2,4), color = 'red'
+#     )
+# )
+# plt.show()
 
