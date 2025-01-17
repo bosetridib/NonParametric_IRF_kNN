fd83b52b239cf47bbfe71aaeba35bf1fba621971
Tridib Bose <84838320+bosetridib@users.noreply.github.com>
2025-01-17 18:17:08 +0200

    ...

--- NonParametricIRF_Forecasting_GIRF_original.py
+++ NonParametricIRF_Forecasting_GIRF_original.py
@@ -17,8 +17,10 @@ df_std = (df - df.mean())/df.std()
 # Horizon "in the middle"
 H = 40
 omega = df_std.iloc[:-1]
+omega = df_std.copy()
 histoi = df_std.iloc[-1]
 
+T = omega.shape[0]
 k = omega.shape[0]
 knn = NearestNeighbors(n_neighbors=k, metric='euclidean')
 
@@ -28,17 +30,45 @@ dist = (dist - dist.min())/(dist.max() - dist.min())
 weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
 # Estimated (NOT forecasted) the period of interest T
 y_f = np.matmul(omega.T, weig).to_frame().T
-y_f = np.matmul(df.iloc[:-1].T, weig).to_frame().T
 
-u = df.iloc[:-1] - y_f.values.squeeze()
+u = omega - y_f.values.squeeze()
 u = u.multiply(weig, axis = 0)
-u.mean()
+sigma_u = np.matmul((u - u.mean()).T , (u - u.mean()).multiply(weig, axis = 0)) / (1 - np.sum(weig**2))
 
-# df.iloc[:-1].plot(
-#     subplots=True, layout=(2,4), color = 'blue',
-#     ax=u.plot(
-#         subplots=True, layout=(2,4), color = 'red'
-#     )
-# )
-# plt.show()
+for i in range(1,H+1):
+    dist = np.array([euclidean(omega.loc[i], histoi) for i in omega.index[i:]])
+    dist = (dist - dist.min())/(dist.max() - dist.min())
+    weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
+    y_f.loc[i] = np.matmul(omega.iloc[i:].T, weig).values
 
+dataplot(y_f)
+
+# Cholesky decomposition
+B_mat = np.linalg.cholesky(sigma_u)
+# Note that sigma_u = residual_cov*((T-1)/(T-Kp-1))
+# The desired shock
+shock = 2
+delta = B_mat[:,shock]
+
+# Estimate y_T_delta
+y_f_delta = pd.DataFrame(columns=y_f.columns)
+y_f_delta.loc[0] = y_f.loc[0] + delta
+
+omega_star = pd.concat([omega, y_f_delta], axis=0)
+
+for i in range(1,H+1):
+    dist = np.array([euclidean(omega_star.loc[i], omega_star) for i in omega_star.index[i:]])
+    dist = (dist - dist.min())/(dist.max() - dist.min())
+    weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
+    y_f_delta.loc[i] = np.matmul(omega.iloc[i:].T, weig).values
+
+dist = np.array([euclidean(omega.loc[i], omega_star) for i in omega.index])
+dist = (dist - dist.min())/(dist.max() - dist.min())
+weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
+# Estimated the period of interest T
+y_f_delta = np.matmul(omega.T, weig).to_frame().T
+
+y_f_delta - y_f
+delta
+
+girf = pd.DataFrame(columns=y_f.columns)
