63a9024a81ea654e28b0b3ac09f98c00ac3f2dfd
Tridib Bose <84838320+bosetridib@users.noreply.github.com>
2025-02-05 18:59:24 +0200

    ...

--- NonParametricIRF_Forecasting_GIRF_original.py
+++ NonParametricIRF_Forecasting_GIRF_original.py
@@ -18,40 +18,40 @@ df = df.dropna()
 # Retrieve the standardized dataset
 
 df_std = (df - df.mean())/df.std()
-df_std = sm.tsa.tsatools.lagmat(df_std, maxlag=6, use_pandas=True)
+# df_std = sm.tsa.tsatools.lagmat(df_std, maxlag=6, use_pandas=True)
 y = df.copy()
 
 # Forecasting
 # Horizon "in the middle"
 H = 40
-omega = df_std.iloc[:-1]
+omega = df_std.iloc[:-(H+1)]
 # omega = df_std.copy()
 histoi = df_std.iloc[-1]
 
 T = omega.shape[0]
-k = omega.shape[0]
-knn = NearestNeighbors(n_neighbors=k, metric='euclidean')
 
-# Estimate y_T
-dist = np.array([euclidean(omega.loc[i], histoi) for i in omega.index])
+knn = NearestNeighbors(n_neighbors=T, metric='euclidean')
+knn.fit(omega)
+dist, ind = knn.kneighbors(histoi.to_numpy().reshape(1,-1))
+dist = dist[0,:]; ind = ind[0,:]
 weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
-# Estimated (NOT forecasted) the period of interest T
-y_f = np.matmul(y.loc[omega.index].T, weig).to_frame().T
+# Estimate y_T
+y_f = np.matmul(y.loc[omega.iloc[ind].index].T, weig).to_frame().T
 
-u = y.loc[omega.index] - y_f.values.squeeze()
+u = y.loc[omega.iloc[ind].index] - y_f.values.squeeze()
 u_mean = u.mul(weig, axis = 0).mean()
 sigma_u = np.matmul((u - u_mean).T, (u - u_mean).mul(weig, axis = 0)) / (1 - np.sum(weig**2))
 
 # y.plot(
 #     subplots=True, layout=(2,4), color = 'blue',
-#     ax=u.plot(
+#     ax=u.sort_index().plot(
 #         subplots=True, layout=(2,4), color = 'red'
 #     )
 # )
 # plt.show()
 
 for h in range(1,H+1):
-    y_f.loc[h] = np.matmul(y.loc[omega.index[h:]].T, weig[:-h]).values
+    y_f.loc[h] = np.matmul(y.loc[omega.iloc[ind].index + pd.DateOffset(months=h)].T, weig).values
 # dataplot(y_f)
 
 # Cholesky decomposition
@@ -67,11 +67,13 @@ y_f_delta.loc[0] = y_f.loc[0] + delta
 histoi_delta = (y_f_delta.loc[0] - df.mean())/df.std()
 histoi_delta = pd.concat([histoi_delta, histoi], axis=0)[:-8]
 
-dist = np.array([euclidean(omega.loc[i], histoi_delta) for i in omega.index])
+knn.fit(omega)
+dist, ind = knn.kneighbors(histoi_delta.to_numpy().reshape(1,-1))
+dist = dist[0,:]; ind = ind[0,:]
 weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
 
 for h in range(1,H+1):
-    y_f_delta.loc[h] = np.matmul(y.loc[omega.index[h:]].T, weig[:-h]).values
+    y_f_delta.loc[h] = np.matmul(y.loc[omega.iloc[ind].index + pd.DateOffset(months=h)].T, weig).values
 # dataplot(y_f_delta)
 
 girf = y_f_delta - y_f
