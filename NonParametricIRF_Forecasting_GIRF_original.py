de4ba5537c71274a83cac899be3c359b56d32d77
bosetridib <bosetridib@yahoo.com>
2025-02-09 18:59:03 +0200

    ...

--- NonParametricIRF_Forecasting_GIRF_original.py
+++ NonParametricIRF_Forecasting_GIRF_original.py
@@ -16,7 +16,7 @@ df = df.dropna()
 # Retrieve the standardized dataset
 
 df_std = (df - df.mean())/df.std()
-# df_std = sm.tsa.tsatools.lagmat(df_std, maxlag=6, use_pandas=True)
+# p=2; df_std = sm.tsa.tsatools.lagmat(df_std, maxlag=p, use_pandas=True).iloc[p:]
 y = pd.concat([epu, cpu, macro_data], axis=1)
 
 # Forecasting
@@ -40,9 +40,9 @@ u = y.loc[omega.iloc[ind].index] - y_f.values.squeeze()
 u_mean = u.mul(weig, axis = 0).mean()
 sigma_u = np.matmul((u - u_mean).T, (u - u_mean).mul(weig, axis = 0)) / (1 - np.sum(weig**2))
 
-# y.plot(
+# y_f.plot(
 #     subplots=True, layout=(2,4), color = 'blue',
-#     ax=u.sort_index().plot(
+#     ax=y_f_delta.plot(
 #         subplots=True, layout=(2,4), color = 'red'
 #     )
 # )
@@ -63,7 +63,7 @@ delta = B_mat[:,shock]
 y_f_delta = pd.DataFrame(columns=y_f.columns)
 y_f_delta.loc[0] = y_f.loc[0] + delta
 histoi_delta = (y_f_delta.loc[0] - y.mean())/y.std()
-histoi_delta = pd.concat([histoi_delta, histoi], axis=0)[:-8]
+histoi_delta = pd.concat([histoi_delta, histoi], axis=0)[:-df.shape[1]]
 
 knn.fit(omega)
 dist, ind = knn.kneighbors(histoi_delta.to_numpy().reshape(1,-1))
@@ -78,31 +78,40 @@ girf = y_f_delta - y_f
 dataplot(girf)
 
 # Confidence Intervals
-R=1000
+R=50
 sim_girf = []
 
 # Perform simulations
 for r in range(0,R):
     omega_resamp = omega.sample(n=T, replace=True).sort_index()
     # Estimate y_T
-    dist = np.array([euclidean(omega_resamp.iloc[i], histoi) for i in range(0,omega_resamp.shape[0])])
+    knn.fit(omega_resamp)
+    dist, ind = knn.kneighbors(histoi.to_numpy().reshape(1,-1))
+    dist = dist[0,:]; ind = ind[0,:]
     weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
-    # Estimated (NOT forecasted) the period of interest T
-    y_f_resamp = y_f.loc[0].to_frame().T
+    # Estimate y_T
+    y_f_resamp = np.matmul(y.loc[omega_resamp.iloc[ind].index].T, weig).to_frame().T
     for h in range(1,H+1):
-        y_f_resamp.loc[h] = np.matmul(y.loc[omega_resamp.index[h:]].T, weig[:-h]).values
-    y_f_delta_resamp = y_f_delta.loc[0].to_frame().T
-    dist = np.array([euclidean(omega_resamp.iloc[i], histoi_delta) for i in range(0,omega_resamp.shape[0])])
+        y_f_resamp.loc[h] = np.matmul(y.loc[omega.iloc[ind].index + pd.DateOffset(months=h)].T, weig).values
+    y_f_delta_resamp = pd.DataFrame(columns=y_f.columns)
+    y_f_delta_resamp.loc[0] = y_f.loc[0] + delta
+    histoi_delta = (y_f_delta.loc[0] - y.mean())/y.std()
+    histoi_delta = pd.concat([histoi_delta, histoi], axis=0)[:-df.shape[1]]
+    knn.fit(omega_resamp)
+    dist, ind = knn.kneighbors(histoi_delta.to_numpy().reshape(1,-1))
+    dist = dist[0,:]; ind = ind[0,:]
     weig = np.exp(-dist**2)/np.sum(np.exp(-dist**2))
     for h in range(1,H+1):
-        y_f_delta_resamp.loc[h] = np.matmul(y.loc[omega_resamp.index[h:]].T, weig[:-h]).values
+        y_f_delta_resamp.loc[h] = np.matmul(y.loc[omega_resamp.iloc[ind].index + pd.DateOffset(months=h)].T, weig).values
+    # dataplot(y_f_delta)
     sim_girf.append(y_f_delta_resamp - y_f_resamp)
+    print('loop: '+str(r))
 # End of loop, and now the sim_list_df has each of the resampled dataframes
 
 conf = 0.90
 # Define the multi-index dataframe for each horizon and CI for each column
 girf_complete = pd.DataFrame(
-    columns = omega.columns,
+    columns = y.columns,
     index = pd.MultiIndex(
         levels=[range(0,H+1),['lower','GIRF','upper']],
         codes=[[x//3 for x in range(0,(H+1)*3)],[0,1,2]*(H+1)], names=('Horizon', 'CI')
@@ -110,19 +119,12 @@ girf_complete = pd.DataFrame(
 )
 
 for h in range(0,H+1):
-    for col in omega.columns:
+    for col in y.columns:
         girf_complete[col][h,'lower'] = 2*girf[col][h] - np.quantile([each_df[col][h] for each_df in sim_girf], conf+(1-conf)/2)
         girf_complete[col][h,'GIRF'] = girf[col][h]
         girf_complete[col][h,'upper'] = 2*girf[col][h] - np.quantile([each_df[col][h] for each_df in sim_girf], (1-conf)/2)
 # girf_complete
 girf_complete = girf_complete.astype('float')
-girf_complete.iloc[:,[2,4,5,6]] = np.exp(girf_complete.iloc[:,[2,4,5,6]].cumsum())
-girf_complete = girf_complete.rename(columns={
-    'Growth_Industrial_Production':'Industrial_Production',
-    'Growth_PriceIndex_Producer':'PriceIndex_Producer',
-    'Growth_PriceIndex_PCE':'PriceIndex_PCE',
-    'Growth_Emission_CO2':'Emission_CO2'
-})
 
 girf_complete = girf_complete.unstack()
 multi_index_col = [(girf_complete.columns[i], girf_complete.columns[i+1], girf_complete.columns[i+2]) for i in range(0,24,3)]
@@ -140,6 +142,7 @@ for i in range(8):
     ax1 = plt.subplot(gs1[i])
     # plt.axis('on')
     ax1.plot(girf_complete[multi_index_col[c][1]])
+    ax1.set_title(y.columns[c])
     c += 1
 plt.tight_layout()
 plt.show()
