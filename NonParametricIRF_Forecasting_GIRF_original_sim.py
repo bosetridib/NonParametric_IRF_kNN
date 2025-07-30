c99e88a2916c5adccb94db1a27388a5466fbf816
bosetridib <bosetridib@yahoo.com>
2025-07-31 01:46:46 +0300

    ...

--- NonParametricIRF_Forecasting_GIRF_original_sim.py
+++ NonParametricIRF_Forecasting_GIRF_original_sim.py
@@ -13,6 +13,18 @@ warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.tsa.api import VAR
 
 # Function to generate time-varying parameters
+def random_walk(n):
+    e_t = np.random.normal(size=n, scale=np.random.randint(low=1, high=5))
+    rw_t = np.zeros(n)
+    rw_t[0] = e_t[0]
+    for i in range(1,n):
+        rw_t[i] = rw_t[i-1]+ e_t[i]
+    
+    return rw_t
+
+n_obs = 10
+B_t = np.matrix([random_walk(n_obs) for _ in range(10)])
+A_t = np.tril([random_walk(n_obs) for _ in range(5)])
+A_t = np.eye(5)
