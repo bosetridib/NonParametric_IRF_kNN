71cc41e928bf3bb8a067a46a26930e5f80266449
bosetridib <bosetridib@yahoo.com>
2026-04-02 02:38:57 +0300

    ...

--- NonParametricIRF_Forecasting_GIRF_original_sim.py
+++ NonParametricIRF_Forecasting_GIRF_original_sim.py
@@ -13,6 +13,7 @@ warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
+from scipy.stats import invwishart, invgamma
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 import warnings
@@ -124,7 +125,7 @@ def tvp_simulate(n_obs = 200, n_var = 2, n_lags = 2, intercept = 1):
 
 test_sim = tvp_simulate()
 np.mean(test_sim['B_mat'][2,:])
-plt.plot(test_sim['B_mat'][7,:]);plt.show()
+# plt.plot(test_sim['B_mat'][7,:]);plt.show()
 dataplot(test_sim['data'])
 
 def tvp_irf(sim_elements, t=20, impulse = 0):
@@ -275,7 +276,7 @@ def knn_irf(sim_elements, t=20, impulse=0):
 
     return {'girf': girf, 'girf_complete': girf_complete}
 
-sim_T = tvp_simulate(100, 3, 2)
+sim_T = tvp_simulate(200, 3, 2)
 tvp_irf(sim_T)
 knn_irf(sim_T)
 # kirf = knn_irf(sim_T)
@@ -286,7 +287,7 @@ knn_irf(sim_T)
 # knn_irf(sim_T)['girf_complete'].loc[1,'GIRF']
 # Begin simulations
 
-n_sim = 50
+n_sim = 25
 bias = []
 
 for n_obs in [_*200 for _ in range(1,6)]:
@@ -298,27 +299,178 @@ for n_obs in [_*200 for _ in range(1,6)]:
             counter -= 1
         else:
             try:
-                for t in range(20, n_obs):
+                for t in range(20, n_obs, 20):
                     knn_irf_sim = knn_irf(sim, t)
                     girf_lwr = knn_irf_sim['girf_complete'].loc[:,(slice(None),'lower')].T.set_index(sim['data'].columns)
                     girf = knn_irf_sim['girf_complete'].loc[:,(slice(None),'GIRF')].T.set_index(sim['data'].columns)
                     girf_upr = knn_irf_sim['girf_complete'].loc[:,(slice(None),'upper')].T.set_index(sim['data'].columns)
                     bias.append({
                         'bias' : girf - tvp_irf(sim).T,
-                        'T': n_obs, 'k': n_var, 'p': n_lags,
+                        'T': n_obs, 't' : t,
                         'ci_l' : girf_lwr, 'ci_u' : girf_upr,
                         'tvp_irf': tvp_irf(sim).T
                     })
                     # There are NaN values generated sometimes randomly, so we remove those simulations
                     if bias[-1]['bias'].isnull().values.any():
                         bias.pop()
+                        print('pop')
                         counter -= 1
+                    # Print the progress
+                    print(str(n_obs) + ',' + str(t) + ',' + str(counter))
             except:
                 counter -= 1
+                print('error')
                 pass
-        print(str(n_obs) + ',' + str(n_var) + ',' +str(n_lags) + ',' +str(counter))
+        # print(str(n_obs) + ',' + str(t) + ',' + str(counter))
         counter += 1
 #End
+
+class TVPVAR(sm.tsa.statespace.MLEModel):
+    # Steps 2-3 are best done in the class "constructor", i.e. the __init__ method
+    def __init__(self, y):
+        # Create a matrix with [y_t' : y_{t-1}'] for t = 2, ..., T
+        augmented = sm.tsa.lagmat(y, 1, trim='both', original='in', use_pandas=True)
+        # Separate into y_t and z_t = [1 : y_{t-1}']
+        p = y.shape[1]
+        y_t = augmented.iloc[:, :p]
+        z_t = sm.add_constant(augmented.iloc[:, p:])
+
+        # Recall that the length of the state vector is p * (p + 1)
+        k_states = p * (p + 1)
+        super().__init__(y_t, exog=z_t, k_states=k_states)
+
+        # Note that the state space system matrices default to contain zeros,
+        # so we don't need to explicitly set c_t = d_t = 0.
+
+        # Construct the design matrix Z_t
+        # Notes:
+        # -> self.k_endog = p is the dimension of the observed vector
+        # -> self.k_states = p * (p + 1) is the dimension of the observed vector
+        # -> self.nobs = T is the number of observations in y_t
+        self['design'] = np.zeros((self.k_endog, self.k_states, self.nobs))
+        for i in range(self.k_endog):
+            start = i * (self.k_endog + 1)
+            end = start + self.k_endog + 1
+            self['design', i, start:end, :] = z_t.T
+
+        # Construct the transition matrix T = I
+        self['transition'] = np.eye(k_states)
+
+        # Construct the selection matrix R = I
+        self['selection'] = np.eye(k_states)
+
+        # Step 3: Initialize the state vector as alpha_1 ~ N(0, 5I)
+        self.ssm.initialize('known', stationary_cov=5 * np.eye(self.k_states))
+
+    # Step 4. Create a method that we can call to update H and Q
+    def update_variances(self, obs_cov, state_cov_diag):
+        self['obs_cov'] = obs_cov
+        self['state_cov'] = np.diag(state_cov_diag)
+
+    # Finally, it can be convenient to define human-readable names for
+    # each element of the state vector. These will be available in output
+    @property
+    def state_names(self):
+        state_names = np.empty((self.k_endog, self.k_endog + 1), dtype=object)
+        for i in range(self.k_endog):
+            endog_name = self.endog_names[i]
+            state_names[i] = (
+                ['intercept.%s' % endog_name] +
+                ['L1.%s->%s' % (other_name, endog_name) for other_name in self.endog_names])
+        return state_names.ravel().tolist()
+
+sim_T = tvp_simulate(200, 3, 2)
+mod = TVPVAR(sim_T['data'])
+initial_obs_cov = np.cov(sim_T['data'].T)
+initial_state_cov_diag = [0.01] * mod.k_states
+
+# Update H and Q
+mod.update_variances(initial_obs_cov, initial_state_cov_diag)
+
+# Perform Kalman filtering and smoothing
+# (the [] is just an empty list that in some models might contain
+# additional parameters. Here, we don't have any additional parameters
+# so we just pass an empty list)
+initial_res = mod.smooth([])
+
+# Gibbs sampler setup
+niter = 11000
+nburn = 1000
+
+# 1. Create storage arrays
+store_states = np.zeros((niter + 1, mod.nobs, mod.k_states))
+store_obs_cov = np.zeros((niter + 1, mod.k_endog, mod.k_endog))
+store_state_cov = np.zeros((niter + 1, mod.k_states))
+
+# 2. Put in the initial values
+store_obs_cov[0] = initial_obs_cov
+store_state_cov[0] = initial_state_cov_diag
+mod.update_variances(store_obs_cov[0], store_state_cov[0])
+
+# 3. Construct posterior samplers
+sim = mod.simulation_smoother(method='cfa')
+
+v10 = mod.k_endog + 3
+S10 = np.eye(mod.k_endog)
+
+# Prior for state cov. variances is inverse-Gamma(v_{i2}^0 / 2 = 3, S+{i2}^0 / 2 = 0.005)
+vi20 = 6
+Si20 = 0.01
+
+for i in range(niter):
+    mod.update_variances(store_obs_cov[i], store_state_cov[i])
+    sim.simulate()
+
+    # 1. Sample states
+    store_states[i + 1] = sim.simulated_state.T
+
+    # 2. Simulate obs cov
+    fitted = np.matmul(mod['design'].transpose(2, 0, 1), store_states[i + 1][..., None])[..., 0]
+    resid = mod.endog - fitted
+    store_obs_cov[i + 1] = invwishart.rvs(v10 + mod.nobs, S10 + resid.T @ resid)
+
+    # 3. Simulate state cov variances
+    resid = store_states[i + 1, 1:] - store_states[i + 1, :-1]
+    sse = np.sum(resid**2, axis=0)
+
+    for j in range(mod.k_states):
+        rv = invgamma.rvs((vi20 + mod.nobs - 1) / 2, scale=(Si20 + sse[j]) / 2)
+        store_state_cov[i + 1, j] = rv
+
+states_posterior_mean = pd.DataFrame(
+    np.mean(store_states[nburn + 1:], axis=0),
+    index=mod._index, columns=mod.state_names)
+def plot_coefficients_by_equation(states):
+    fig, axes = plt.subplots(2, 2, figsize=(15, 8))
+
+    # The way we defined Z_t implies that the first 5 elements of the
+    # state vector correspond to the first variable in y_t, which is GDP growth
+    ax = axes[0, 0]
+    states.iloc[:, :5].plot(ax=ax)
+    ax.set_title('GDP growth')
+    ax.legend()
+
+    # The next 5 elements correspond to inflation
+    ax = axes[0, 1]
+    states.iloc[:, 5:10].plot(ax=ax)
+    ax.set_title('Inflation rate')
+    ax.legend();
+
+    # The next 5 elements correspond to unemployment
+    ax = axes[1, 0]
+    states.iloc[:, 10:15].plot(ax=ax)
+    ax.set_title('Unemployment equation')
+    ax.legend()
+
+    # The last 5 elements correspond to the interest rate
+    ax = axes[1, 1]
+    states.iloc[:, 15:20].plot(ax=ax)
+    ax.set_title('Interest rate equation')
+    ax.legend();
+
+    return ax
+# Plot these means over time
+plot_coefficients_by_equation(states_posterior_mean); plt.show()
 
 import pickle
 # Saving objects:
@@ -367,16 +519,16 @@ rmse_avg_var.plot();plt.show()
 # Separate bias list by n_obs
 # n_obs: 200 = n_var:2,3,4 & 200 obs used in n_lags: 1,2,3,4; and so on
 # so, each n_obs is repeated
-bias_avg_T = [[_ for _ in bias if _['T'] == count*200] for count in range(1,6)]
+bias_avg_T = [[_ for _ in bias if _['T'] == count*200] for count in range(1,4)]
 bias_avg_T = [[np.absolute(b['bias']).mean() for b in _] for _ in bias_avg_T]
 bias_avg_T = [sum(_)/len(_) for _ in bias_avg_T]
-bias_avg_T = pd.DataFrame([_ for _ in bias_avg_T], index=[_*200 for _ in range(1,6)]).T
+bias_avg_T = pd.DataFrame([_ for _ in bias_avg_T], index=[_*200 for _ in range(1,4)]).T
 bias_avg_T.plot(); plt.show()
 
-rmse_avg_T = [[_ for _ in bias if _['T'] == count*200] for count in range(1,6)]
+rmse_avg_T = [[_ for _ in bias if _['T'] == count*200] for count in range(1,4)]
 rmse_avg_T = [[np.absolute(b['bias']).mean()**2 for b in _] for _ in rmse_avg_T]
 rmse_avg_T = [(sum(_)/len(_))**0.5 for _ in rmse_avg_T]
-rmse_avg_T = pd.DataFrame([_ for _ in rmse_avg_T], index=[_*200 for _ in range(1,6)]).T
+rmse_avg_T = pd.DataFrame([_ for _ in rmse_avg_T], index=[_*200 for _ in range(1,4)]).T
 rmse_avg_T.plot(); plt.show()
 
 # Separate bias list by n_lags
@@ -400,7 +552,7 @@ tab_bias = tab.copy()
 tab_rmse = tab.copy()
 tab_CR = tab.copy()
 
-tab_CR_T = pd.DataFrame(columns=range(0,11), index=[_*200 for _ in range(1,6)])
+tab_CR_T = pd.DataFrame(columns=range(0,11), index=[_*200 for _ in range(1,4)])
 
 for n_var in range(2,5):
     for n_lags in range(1,5):
@@ -416,10 +568,10 @@ for n_var in range(2,5):
             coverage_h = [(b['ci_l'][h].values < b['tvp_irf'][h].values) & (b['tvp_irf'][h].values <= b['ci_u'][h].values) for b in bias if (b['k'] == n_var) & (b['p'] == n_lags)]
             tab_CR.loc[(n_var, n_lags), h] = np.mean(coverage_h)
 
-for n_obs in [_*200 for _ in range(1,6)]:
+for n_obs in [_*200 for _ in range(1,4)]:
     for h in range(0,11):
         coverage_h_T = [np.mean((b['ci_l'][h].values < b['tvp_irf'][h].values) & (b['tvp_irf'][h].values <= b['ci_u'][h].values)) for b in bias if (b['T'] == n_obs)]
-        tab_CR_T[h].loc[n_obs] = np.mean(coverage_h_T)
+        tab_CR_T.loc[n_obs, h] = np.mean(coverage_h_T)
 
 
 print(tab_bias[[0,2,8]].to_latex(float_format="%.2f"))
